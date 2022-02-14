dadbbf45217138b4fa11ec17a1330e7c6df2e8fa
Crytelhin <matushnyaK@mail.ru>
2022-02-14 21:16:18 +0200

    Implement Increment App

---
+++ IncrementerApp1/Controllers/IncrementerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using IncrementerApp1.Domain;
+
+namespace IncrementerApp1.Controllers
+{
+    [ApiController]
+    [Route("incrementer")]
+    public class IncrementerController : Controller
+    {
+        
+        [HttpGet]
+        public ActionResult<IncrementerResponse> Increment()
+        {
+            int currentValue = Request.Cookies["CurrentValue"] != null
+                ? Convert.ToInt32(Request.Cookies["CurrentValue"]) + 1 : 0;
+            Response.Cookies.Append("CurrentValue", $"{currentValue}");
+            
+            return Ok(new IncrementerResponse(currentValue));
+        }
+        
+    }
+}
---
+++ IncrementerApp1/Domain/IncrementerResponse.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IncrementerApp1.Domain
+{
+    public class IncrementerResponse
+    {
+        public int currentValue { get; set; }
+
+        public IncrementerResponse(int currentValue)
+        {
+            this.currentValue = currentValue;
+        }
+
+    }
+
+}
